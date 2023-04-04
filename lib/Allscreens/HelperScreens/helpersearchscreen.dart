68885bdeaeccbd062eac5ec77f9e53bd50821e20
Soumesh Mohanty <soumeshmohanty0220@gmail.com>
2023-04-04 16:08:38 +0530

    helper locations save to database

--- lib/Allscreens/HelperScreens/helpersearchscreen.dart
+++ lib/Allscreens/HelperScreens/helpersearchscreen.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: use_build_context_synchronously, deprecated_member_use, depend_on_referenced_packages, library_private_types_in_public_api
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
 
 class CurrentPathPage extends StatefulWidget {
   const CurrentPathPage({Key? key}) : super(key: key);
@@ -8,97 +14,124 @@ class CurrentPathPage extends StatefulWidget {
 }
 
 class _CurrentPathPageState extends State<CurrentPathPage> {
-  final _formKey = GlobalKey<FormState>();
-  final _locationController = TextEditingController();
-  final _destinationController = TextEditingController();
-  final _timeController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final DatabaseReference _database = FirebaseDatabase.instance.reference();
+
+  TextEditingController currentLocationController = TextEditingController();
+  TextEditingController destinationController = TextEditingController();
+  TextEditingController timeController = TextEditingController();
+
+  Future<void> _savePath() async {
+    final User? user = _auth.currentUser;
+    if (user == null) {
+      // If the user is not logged in, do not save the path.
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('You must be logged in to save the path')),
+      );
+      return;
+    }
+
+    String currentLocation = currentLocationController.text.trim();
+    String destination = destinationController.text.trim();
+    String time = timeController.text.trim();
+
+    if (currentLocation.isEmpty || destination.isEmpty || time.isEmpty) {
+      // show an error message if any of the fields are empty
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('All fields are required')),
+      );
+      return;
+    }
+
+    try {
+      // get the current position using Geolocator package
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      double lat = position.latitude;
+      double long = position.longitude;
+
+      // save the path to Firebase Realtime Database
+      _database.child('users').child(user.uid).child('paths').set({
+        'currentLocation': currentLocation,
+        'destination': destination,
+        'time': time,
+        'latitude': lat,
+        'longitude': long,
+      });
+
+      // show a success message
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Path added successfully')),
+      );
+
+      // clear the text fields
+      currentLocationController.clear();
+      destinationController.clear();
+      timeController.clear();
+    } catch (e) {
+      print(e);
+      // show an error message if any error occurs
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error adding path')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Current Path'),
+        title: const Text('Add Path'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Location:',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              controller: _locationController,
-              decoration: InputDecoration(
-                hintText: 'Enter location',
-                border: OutlineInputBorder(),
+            TextField(
+              controller: currentLocationController,
+              decoration: const InputDecoration(
+                labelText: 'Current location',
+                hintText: 'Enter your current location',
+                icon: Icon(Icons.location_on, color: Colors.blue,size: 30,),
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter location';
+            ),
+            TextField(
+              controller: destinationController,
+              decoration: const InputDecoration(
+                labelText: 'Destination',
+                hintText: 'Enter your destination location',
+                icon: Icon(Icons.location_on, color: Colors.blue,size: 30,),
+              ),
+            ),
+            TextField(
+              controller: timeController,
+              decoration: const InputDecoration(
+                labelText: 'Time of leaving (hh:mm)',
+                icon: Icon(Icons.lock_clock, color: Colors.blue,size: 30,),
+              ),
+              keyboardType: TextInputType.datetime,
+              onTap: () async {
+                // show a time picker when the user taps on the time field
+                TimeOfDay? selectedTime = await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now(),
+                );
+                if (selectedTime != null) {
+                  timeController.text =
+                      DateFormat('HH:mm').format(DateTime(
+                    0,
+                    0,
+                    0,
+                    selectedTime.hour,
+                    selectedTime.minute,
+                  ));
                 }
-                return null;
               },
             ),
-            SizedBox(height: 20),
-            Text(
-              'Destination:',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              controller: _destinationController,
-              decoration: InputDecoration(
-                hintText: 'Enter destination',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter destination';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Out Time:',
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              controller: _timeController,
-              decoration: InputDecoration(
-                hintText: 'Enter time',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter time';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 30),
-            SizedBox(
-              width: double.infinity,
-              height: 40.0,
-              child: ElevatedButton(
-                onPressed: () {
-                  {
-                    Navigator.pop(context);
-                  }
-                },
-                child: Text(
-                  'Save',
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: _savePath,
+              child: const Text('Save'),
             ),
           ],
         ),
